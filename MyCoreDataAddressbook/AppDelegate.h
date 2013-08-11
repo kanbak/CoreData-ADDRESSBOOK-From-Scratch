f43caa75822888887c470b1f8485cc15e200004d
Umut Kanbak <uk@kanbak.net>
2013-08-11 15:35:18 -0500

    Initial Commit

---
+++ MyCoreDataAddressbook/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MyCoreDataAddressbook
+//
+//  Created by Umut Kanbak on 8/11/13.
+//  Copyright (c) 2013 Umut Kanbak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
